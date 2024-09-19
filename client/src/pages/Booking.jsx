6e67167e1d5e5262f7c92c2e46942e82ecfbfeb4
millan-figueroa <millan.fig@gmail.com>
2024-09-19 14:55:24 -0700

    Feat: add service & groomer selection with calendar - useState for form control - react-calendar for booking - useNavigate for post-booking redirect - Static service/groomer options, future MongoDB support

--- client/src/pages/Booking.jsx
+++ client/src/pages/Booking.jsx
@@ -1,5 +1,29 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+//Import useState to capture customer selections and control form data.
+import React, { useState } from 'react';
+
+//Import react-calendar to create a ready-made interactive calendar.
+import Calendar from 'react-calendar';
+
+//Import useNavigate to redirect the user back to the Home page after booking.
+import { useNavigate } from 'react-router-dom';
+
+//Define variables for dropdown menus. These could be saved in MongoDB instead to make them updateable in a UI element.
+const services = [
+    'Small dog bath (less than 20lbs)',
+    'Medium dog bath (21 - 40lbs)',
+    'Large dog bath (41 - 60 lbs)',
+    'XLarge dog bath (61+ lbs)',
+    'Cat bath short coat', 'Cat bath long coat',
+    'Nail trim', 'Haircut', 'Deluxe Package'
+];
+
+const groomers = [
+    'Che Guevara',
+    'Simón Bolívar',
+    'Isabel Allende',
+    'Jose Artigas'
+]
 
 const Booking = () => {
 
