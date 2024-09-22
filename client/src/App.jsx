d41640e8c0e50cfd3845d2316237308847b2f203
millan-figueroa <millan.fig@gmail.com>
2024-09-21 20:45:48 -0700

    Refactor footer html, remove dropdown functionality in nav, updated footer and info

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
-//Create a global header for all pages on the site, and render the target of the clicked route as the body of the page.
+//Global header and footer
 
 const App = () => {
   return (
@@ -13,21 +14,25 @@ const App = () => {
           <div id="logo">
             <h1><a href="/">Furry Godmother</a></h1>
           </div>
+
+          {/* Nav */}
           <nav id="nav">
             <ul>
-              <li><a href="/">Home</a></li>
-              <li>
-                <a href="#">Services</a>
-                <ul>
-                  <li><a href="#">Grooming</a></li>
-                  <li><a href="#">Cuts</a></li>
-                  <li><a href="#">Spa</a></li>
-                  <li><a href="#">Extras</a></li>
-                </ul>
+              <li className="current" style={{ whiteSpace: 'nowrap' }}>
+                <Link to="/">Welcome</Link>
               </li>
-              <li><a href="/booking">Booking</a></li>
-              <li><a href="/staff">Staff</a></li>
-              <li><a href="/admin">Admin</a></li>
+              <li style={{ whiteSpace: 'nowrap' }}>
+                <Link to="/services">Services</Link>
+              </li>              
+              <li style={{ whiteSpace: 'nowrap' }}>
+                <Link to="/booking">Booking</Link>
+              </li>
+              <li style={{ whiteSpace: 'nowrap' }}>
+                <Link to="/staff">Staff</Link>
+              </li>
+              <li style={{ whiteSpace: 'nowrap' }}>
+                <Link to="/admin">Admin</Link>
+              </li>
             </ul>
           </nav>
         </header>
@@ -35,48 +40,83 @@ const App = () => {
 
       <div id="main-wrapper">
         <main>
-          <Outlet /> {/* This is where your page content will render */}
+          <Outlet /> {/* Render appropriate page */}
         </main>
       </div>
 
       <div id="footer-wrapper">
-        <footer id="footer" className="container">
-          <div className="row">
-            <div className="col-3 col-6-medium col-12-small">
-              <section className="widget links">
-                <h3>Random Stuff</h3>
-                <ul className="style2">
-                  <li><a href="#">Etiam feugiat condimentum</a></li>
-                  <li><a href="#">Aliquam imperdiet suscipit odio</a></li>
-                  <li><a href="#">Sed porttitor cras in erat nec</a></li>
-                  <li><a href="#">Felis varius pellentesque potenti</a></li>
-                  <li><a href="#">Nullam scelerisque blandit leo</a></li>
-                </ul>
-              </section>
-            </div>
-            <div className="col-3 col-6-medium col-12-small">
-              <section className="widget contact last">
-                <h3>Contact Us</h3>
-                <ul>
-                  <li><a href="#" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
-                  <li><a href="#" className="icon brands fa-facebook-f"><span className="label">Facebook</span></a></li>
-                  <li><a href="#" className="icon brands fa-instagram"><span className="label">Instagram</span></a></li>
-                </ul>
-                <p>1234 Fictional Road<br />Nashville, TN 00000<br />(800) 555-0000</p>
-              </section>
+      <footer id="footer" className="container">
+        <div className="row">
+          <div className="col-3 col-6-medium col-12-small">
+            {/* Links Section */}
+            <section className="widget links">
+              <h3>QUICK LINKS</h3>
+              <ul className="style2">
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Booking</a></li>
+                <li><a href="#">Staff</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Employment</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+              </ul>
+            </section>
+          </div>
+          <div className="col-3 col-6-medium col-12-small">
+
+            {/* Schedule and Contacts */}
+            <section className="widget links">
+              <h3>SCHEDULE</h3>
+              <ul className="style2">
+                <li><a href="#">Monday CLOSED</a></li>
+                <li><a href="#">Tue - Fri 9am - 6pm</a></li>
+                <li><a href="#">Sat & Sun 9am - 4pm</a></li>
+                <li><a href="#">Call us for holiday hours!</a></li>
+              </ul>
+            </section>
+          </div>
+          <div className="col-3 col-6-medium col-12-small">
+
+            {/* Yet Another Links Section */}
+            <section className="widget links">
+              <h3>LOCATIONS</h3>
+              <ul className="style2">
+                <li><a href="#">East LA/ Montebello</a></li>
+                <li><a href="#">Beverlywood</a></li>
+                <li><a href="#">DTLA</a></li>
+                <li><a href="#">Culver City</a></li>
+                <li><a href="#">Riverside</a></li>
+              </ul>
+            </section>
+          </div>
+          <div className="col-3 col-6-medium col-12-small">
+
+            {/* Contact Section */}
+            <section className="widget contact last">
+              <h3>CONTACT US</h3>
+              <p>info@furrygodmother.com<br />
+              (323)459-1291</p>
+              <ul>
+                <li><a href="#" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
+                <li><a href="#" className="icon brands fa-facebook-f"><span className="label">Facebook</span></a></li>
+                <li><a href="#" className="icon brands fa-instagram"><span className="label">Instagram</span></a></li>
+                <li><a href="#" className="icon brands fa-dribbble"><span className="label">Dribbble</span></a></li>
+                <li><a href="#" className="icon brands fa-pinterest"><span className="label">Pinterest</span></a></li>
+              </ul>
+              
+            </section>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <div id="copyright">
+              <ul className="menu">
+                <li>&copy; Furry Godmother Pet Spa. All rights reserved</li>
+                <li>Design: <a href="http://diruptdev.netlify.app">DisruptDev</a></li>
+              </ul>
             </div>
           </div>
-          <div className="row">
-            <div className="col-12">
-              <div id="copyright">
-                <ul className="menu">
-                  <li>&copy; Furry Godmother Pet Spa. All rights reserved</li>
-                  <li>Design: <a href="http://disruptdev.netlify.app">DisruptDev</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </footer>
+        </div>
+      </footer>
       </div>
     </div>
   );
