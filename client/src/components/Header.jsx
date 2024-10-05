41c9fbb8815cbb61c0bbf3194f91a0505439aae9
millan-figueroa <millan.fig@gmail.com>
2024-10-05 12:12:34 -0700

    Fix header logo alignment and size issues

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,19 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
-import Logo from '../assets/css/images/furry_godmother_logo.svg';
+import Logo from "../assets/css/images/furry_godmother_logo.png";
+
+
 
 const Header = () => {
     // State to manage the theme
     const [isDarkMode, setIsDarkMode] = useState(false);
-      const headerWrapper = document.getElementById('header-wrapper');
-      const pageWrapper = document.getElementById('page-wrapper');
-
 
     // Effect to apply the theme
     useEffect(() => {
-      headerWrapper.style.backgroundColor = isDarkMode ? '#645069' : '#FEDEDF';
-      // pageWrapper.style.color = isDarkMode ? '#FFF' : '#000';
+        document.body.style.backgroundColor = isDarkMode ? '#333' : '#FFF';
+        document.body.style.color = isDarkMode ? '#FFF' : '#000';
     }, [isDarkMode]); // Runs when isDarkMode changes
 
     // Function to toggle the theme
@@ -24,11 +23,10 @@ const Header = () => {
 
     return (
         <div id="header-wrapper">
-          <header id="header" className="container">
-            <div id="logo">
-              <img src={Logo} alt="Logo" />
-            </div>
-
+        <header id="header" className="container">
+          <div id="logo">
+          <img src={Logo} alt="Logo" />
+          </div>
 
           {/* Nav */}
           <nav id="nav">
@@ -50,15 +48,19 @@ const Header = () => {
               </li>
               <li style={{ whiteSpace: 'nowrap' }}>
                 {/* Theme Toggle Button */}
-                <button onClick={toggleTheme} style={{ marginTop: '5px', background: 'none', border: 'none', cursor: 'pointer' }}>
-                    <FontAwesomeIcon icon={isDarkMode ? faSun : faMoon} size="lg" />
-                </button>
+            <button onClick={toggleTheme} style={{ marginTop: '5px', background: 'none', border: 'none', cursor: 'pointer' }}>
+                <FontAwesomeIcon icon={isDarkMode ? faSun : faMoon} size="lg" />
+            </button>
               </li>
             </ul>
+            
+            
           </nav>
         </header>
       </div>
+
     );
+
 };
 
 export default Header;
